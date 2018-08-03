812cdf7ebf712b84ce58d6e282a679711475b782
Wonjun Jang <strutive07@gmail.com>
2018-08-03 14:34:17 +0900

    Update ngrams.py

--- stacked_lstm/ngrams.py
+++ stacked_lstm/ngrams.py
@@ -12,8 +12,8 @@ from collections import Counter
 import pickle
 import os, glob
 
-global afdata
-afdata = {};
+class SmallerThenTrigram(Exception):
+    pass
 
 def get_ngram(text, n):
     print("get_ngram()")
@@ -75,6 +75,50 @@ def trigram_to_bigram_LR(tri):
             toLeftDict[rightTok] = tmp_list
     return toRightDict, toLeftDict
 
+def ngram_to_bigram_LR(n,ngram):
+    if n < 3:
+        raise SmallerThenTrigram()
+    toRightDict = {}
+    toLeftDict = {}
+    for l in ngram:
+        leftTok = (l[0][0], l[0][1])
+        rightTok = (l[0][n-2], l[0][n-1])
+        if leftTok in toRightDict:
+            toRightDict[leftTok].append(l)
+        else:
+            tmp_list = [l]
+            toRightDict[leftTok] = tmp_list
+
+        if rightTok in toLeftDict:
+            toLeftDict[rightTok].append(l)
+        else:
+            tmp_list = [l]
+            toLeftDict[rightTok] = tmp_list
+    return toRightDict, toLeftDict
+
+def ngram_to_trigram_LR(n,ngram):
+    if n < 4:
+        raise SmallerThenTrigram()
+    toRightDict = {}
+    toLeftDict = {}
+    for l in ngram:
+        leftTok = (l[0][0], l[0][1], l[0][2])
+        rightTok = (l[0][n-3], l[0][n-2], l[0][n-1])
+        if leftTok in toRightDict:
+            toRightDict[leftTok].append(l)
+        else:
+            tmp_list = [l]
+            toRightDict[leftTok] = tmp_list
+
+        if rightTok in toLeftDict:
+            toLeftDict[rightTok].append(l)
+        else:
+            tmp_list = [l]
+            toLeftDict[rightTok] = tmp_list
+    return toRightDict, toLeftDict
+
+
+
 def main():
     isSaved = True
 
@@ -82,21 +126,31 @@ def main():
         os.chdir("./next_data")
         global tri_fdist
         global bi_fdist
+        global four_fdist, five_fdist
         tri_fdist = get_ngrams_saved(3)
         bi_fdist = get_ngrams_saved(2)
+        four_fdist = get_ngrams_saved(4)
+        five_fdist = get_ngrams_saved(5)
     else:
         data = ""
-        #     os.chdir("./next_data")
+        os.chdir("./next_data")
         for file in glob.glob("*.txt"):
             data += open(file, encoding='utf-8').read().lower()
         #     get_ngram(data,2)
 
         fdist, ngrams = get_ngram(data, 3)
         bi_fdist, bigrams = get_ngram(data, 2)
+
     tri_flist = tri_fdist.most_common(len(tri_fdist))
-    tri_dict_data = create_ngram_dict(tri_flist, 3, 50)
-    toRight, toLeft = trigram_to_bigram_LR(tri_flist)
+    four_flist = four_fdist.most_common(len(four_fdist))
+    five_flist = five_fdist.most_common(len(five_fdist))
+    
+    toRight_tri2bi, toLeft_tri2bi = ngram_to_bigram_LR(3, tri_flist)
+    toRight_four2bi, toLeft_four2bi = ngram_to_bigram_LR(4, four_flist)
+    toRight_five2bi, toLeft_five2bi = ngram_to_bigram_LR(5, five_flist)
+    
+    toRight_four2tri, toLeft_four2tri = ngram_to_trigram_LR(4, four_flist)
+    toRight_five2tri, toLeft_five2tri = ngram_to_trigram_LR(5, five_flist)
+    
     # toRight : X, Y 나오고 그 다음에 나올 것
     # toLeft : A, X , Y 에서 X, Y 나왔을 때 A 에 올것.
-if __name__ == '__main__':
-    main()
