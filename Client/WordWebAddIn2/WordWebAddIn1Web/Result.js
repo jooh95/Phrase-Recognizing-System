7910b1eb9eb35874830bb9b75d24e52690fef204
Oh Joonoh <jack.ohjoonoh@gmail.com>
2018-11-10 21:41:25 +0900

    drag error fix & recommending word update

--- Client/WordWebAddIn2/WordWebAddIn1Web/Result.js
+++ Client/WordWebAddIn2/WordWebAddIn1Web/Result.js
@@ -2,7 +2,6 @@
 
     "use strict";
     var messageBanner;
-   
 
     // 새 페이지가 로드될 때마다 초기화 함수가 실행되어야 합니다.
     Office.initialize = function (reason) {
@@ -13,9 +12,9 @@
                 //채워야함.
                 return;
             }
-
+            
+            
             $('#rec_button').click(recommendSentence);
-
             $('#hist_button').click(showHistory);
         });
     };
@@ -32,8 +31,8 @@
         var xmlhttp = new XMLHttpRequest();
 
         // Or post, etc
-        xmlhttp.open("GET", proxy + myUrl + '?key=k&q=' + dragged + '&source=en&target=ko&format=text', true);
-
+        xmlhttp.open("GET", proxy + myUrl + '?key=apikey&q=' + dragged + '&source=en&target=ko&format=text', true);
+        xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         xmlhttp.send();
 
         xmlhttp.onload = function () {
@@ -54,14 +53,51 @@
     }
 
     function sendToNaver(dragged) {
-        $('#r_naver').html('<h3>Naver</h3><ul></ul>');
+        $('#r_naver').html('<h3>Kakao</h3><ul></ul>');
 
-        var client_id = 'k';
-        var client_secret = 'k';
+        var client_id = 'apikey';
+        //var client_secret = '1IjcbfdwKl';
 
-        var query = dragged;
+        var myUrl = "https://kapi.kakao.com/v1/translation/translate";
+        //  But if you make it from a browser, then it will work without problem ...
 
-        var myUrl = 'https://openapi.naver.com/v1/papago/n2mt';
+        //// However to make it work, we are going to use the cors-anywhere free service to bypass this
+        var proxy = "https://cors-anywhere.herokuapp.com/";
+
+        // Execute request
+        var xmlhttp = new XMLHttpRequest();
+
+        // Or post, etc
+        xmlhttp.open("GET", proxy + myUrl + '?src_lang=en&target_lang=kr&query=' + dragged);
+        xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xmlhttp.setRequestHeader('Authorization', client_id);
+        xmlhttp.send();
+        //var jsonRequest = { source: 'en', target: 'ko', text: query };
+
+        //xmlhttp.send(JSON.stringify(jsonRequest));
+
+        xmlhttp.onload = function () {
+
+            console.log(this.responseText);
+
+            var jsonData = JSON.parse(this.responseText);
+            //console.log(jsonData["message"]["result"]["translatedText"]);
+            //for (var i = 0; i < jsonData["message"]["result"].length; i++) {
+            //    var word = jsonData["data"]["translations"][i]["translatedText"];
+            //    console.log("##########");
+            //    console.log(word);
+            //    console.log("##########");
+            //    $('#r_naver ul').append("<li><label id = label" + i + ">" + word + '</label></li>');
+            //}
+            var word = jsonData["translated_text"];
+            $('#r_naver ul').append("<li><label>" + word + '</label></li>');
+
+        };
+    }
+
+
+    function translateVocab(word, pos) {
+        var myUrl = 'https://translation.googleapis.com/language/translate/v2';
         //  But if you make it from a browser, then it will work without problem ...
 
         //// However to make it work, we are going to use the cors-anywhere free service to bypass this
@@ -71,39 +107,122 @@
         var xmlhttp = new XMLHttpRequest();
 
         // Or post, etc
-        xmlhttp.open("POST", proxy + myUrl);
-        xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-        xmlhttp.setRequestHeader('X-Requested-With', 'xmlhttprequest');
-        xmlhttp.setRequestHeader('Content-Type', 'application/json');
-        xmlhttp.setRequestHeader('X-Naver-Client-Id', client_id);
-        xmlhttp.setRequestHeader('X-Naver-Client-Secret', client_secret);
+        xmlhttp.open("GET", proxy + myUrl + '?key=AIzaSyBCXxskVOciO7tlqD7-RR56MrFQGitL-30&q=' + word + '&source=en&target=ko&format=text', true);
+        xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xmlhttp.send();
 
-        var jsonRequest = { source: 'en', target: 'ko', text: query };
+            xmlhttp.onload = function () {
 
-        xmlhttp.send(JSON.stringify(jsonRequest));
+                var jsonData = JSON.parse(this.responseText);
 
-        xmlhttp.onload = function () {
-            // do something to response
-            //console.log(this.responseText);
-            
-            var jsonData = JSON.parse(this.responseText);
-            console.log(jsonData["message"]["result"]["translatedText"]);
-            //for (var i = 0; i < jsonData["message"]["result"].length; i++) {
-            //    var word = jsonData["data"]["translations"][i]["translatedText"];
-            //    console.log("##########");
-            //    console.log(word);
-            //    console.log("##########");
-            //    $('#r_naver ul').append("<li><label id = label" + i + ">" + word + '</label></li>');
-            //}
-            var word = jsonData["message"]["result"]["translatedText"];
-            $('#r_naver ul').append("<li><label>" + word + '</label></li>');
+                var word = jsonData["data"]["translations"][0]["translatedText"];
 
-        };
+                console.log(pos);
+                $('#button' + pos).append(" : " + word);
+            };
+
+        
     }
 
-    function sendToServer(dragged, whole_text) {
-        $('#r_result').html('<h3>Naver</h3><ul></ul>');
-        var myUrl = 'http://52.79.106.90:5000/dev/data';
+    function getRecommendList(dragged) {
+        Word.run(function (context) {
+            return context.sync()
+                .then(function () {
+                    
+
+                    var whole_text = $("#tmp_content").text();
+
+                    var myUrl = 'http://mpbear11.duckdns.org:5000/dev/v3/data';
+                    //  But if you make it from a browser, then it will work without problem ...
+
+                    // However to make it work, we are going to use the cors-anywhere free service to bypass this
+                    var proxy = 'https://cors-anywhere.herokuapp.com/';
+
+                    // Execute request
+                    var xmlhttp = new XMLHttpRequest();
+
+                    // Or post, etc
+                    xmlhttp.open("POST", proxy + myUrl, true);
+                    xmlhttp.setRequestHeader("Content-Type", "application/json");
+                    xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+                    console.log(JSON.stringify({ full_sentence: whole_text, target: dragged }));
+                    //xmlhttp.send(JSON.stringify({ full_sentence: whole_text, target: "<31fe0826-7bb3-444d-8f2e-6ca06ae24e11>" + dragged }));
+                    xmlhttp.send(JSON.stringify({ full_sentence: whole_text, target: "<31fe0826-7bb3-444d-8f2e-6ca06ae24e11>" + dragged }));
+                    //xmlhttp.send(sentnece);
+                    xmlhttp.onload = function () {
+                        // do something to response
+                        console.log("response");
+                        console.log(JSON.parse(this.responseText));
+
+                        var jsonData = JSON.parse(this.responseText);
+
+                        var count = 0;
+
+                        var drag = dragged;
+                        $('#r_result ul').html("");
+                        for (var i = 0; i < jsonData["result"].length; i++) {
+                            var word = jsonData["result"][i]["target"];
+                            console.log(word);
+                            $('#r_result ul').append("<li><button type='button' class='btn btn-primary' id = button" + i + ">" + word + "</button></li>");
+
+                            $("#button" + i).on("click", { word: word, dragged: dragged }, changeWord);
+
+                            translateVocab(word, i);
+
+                            count++;
+
+                        }
+                  
+                        for (var i = 0; i < jsonData["word2vec"].length; i++) {
+                            var word = jsonData["word2vec"][i]["word"];
+                            console.log(word);
+                            $('#r_result ul').append("<li><button type='button' class='btn btn-primary' id = button" + (i + count) + ">" + word + '</button></li>');
+
+                            $("#button" + (i + count)).on("click", { word: word, dragged: dragged }, changeWord);
+
+                            translateVocab(word, i + count);
+
+                            count++;
+
+                        }
+                    };
+                });
+        })
+        .catch(errorHandler);
+
+       
+    }
+
+    function sendHistory(dragged, changed) {
+        
+        Word.run(function (context) {
+            
+            return context.sync()
+                .then(function () {
+                    //$('#r_result').html('<h3>Naver</h3><ul></ul>');
+                    var myUrl = 'http://mpbear11.duckdns.org:5000/history/push';
+                    //  But if you make it from a browser, then it will work without problem ...
+
+                    // However to make it work, we are going to use the cors-anywhere free service to bypass this
+                    var proxy = 'https://cors-anywhere.herokuapp.com/';
+
+                    // Execute request
+                    var xmlhttp = new XMLHttpRequest();
+
+                    // Or post, etc
+
+                    xmlhttp.open("POST", proxy + myUrl, true);
+                    xmlhttp.setRequestHeader("Content-Type", "application/json");
+                    xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+                    xmlhttp.send(JSON.stringify({ user_name: "test1234", before: dragged, after: changed }));
+                    //xmlhttp.send(sentnece);
+                });
+        })
+        .catch(errorHandler);
+    }
+
+    function receiveHistory(dragged, changed) {
+        var myUrl = 'http://mpbear11.duckdns.org:5000/history/get';
         //  But if you make it from a browser, then it will work without problem ...
 
         // However to make it work, we are going to use the cors-anywhere free service to bypass this
@@ -115,49 +234,56 @@
         // Or post, etc
         xmlhttp.open("POST", proxy + myUrl, true);
         xmlhttp.setRequestHeader("Content-Type", "application/json");
-        console.log(JSON.stringify({ full_sentence: whole_text, target: "<31fe0826-7bb3-444d-8f2e-6ca06ae24e11>" + dragged }));
-        xmlhttp.send(JSON.stringify({ full_sentence: whole_text, target: "<31fe0826-7bb3-444d-8f2e-6ca06ae24e11>" + dragged}));
+        xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        xmlhttp.send(JSON.stringify({ user_name: "test1234" }));
         //xmlhttp.send(sentnece);
+
+        $('#history_main').html("");
+
         xmlhttp.onload = function () {
             // do something to response
             //console.log(JSON.parse(this.responseText)["result"][0]["target"]["word"]);
 
+            console.log("222222");
+            console.log(this.responseText);
             var jsonData = JSON.parse(this.responseText);
 
-            var count = 0;
-
             for (var i = 0; i < jsonData["result"].length; i++) {
-                var word = jsonData["result"][i]["target"]["word"];
-                console.log(word);
-                $('#r_result ul').append("<li><button type='button' class='btn btn-primary' id = button" + i + ">" + word + '</button></li>');
-
-                $("#button" + i).on("click", { word: word }, changeWord);
-                count++;
+                var before = jsonData["result"][i]["before"];
+                var after = jsonData["result"][i]["after"];
+                $('#history_main').append("<div><ul><li> 원래 단어 : " + before + " -> 수정한 단어 : " + after + "</div></li></ul>");
             }
 
-            for (var i = 0; i < jsonData["word2vec"].length; i++) {
-                var word = jsonData["word2vec"][i]["word"];
-                console.log(word);
-                $('#r_result ul').append("<li><button type='button' class='btn btn-primary' id = button" + (i + count) + ">" + word + '</button></li>');
-
-                $("#button" + (i + count)).on("click", { word: word }, changeWord);
-            }
-            
         };
+
     }
 
     function changeWord(event) {
         Word.run(function (context) {
+
+            var whole_text = $("#tmp_content").text();
+            var dragged = context.document.getSelection();
+            //context.load(dragged, 'text');
+            context.document.body.clear();
+
+            whole_text = whole_text.replace($("#tmp_dragged").text(), event.data.word);
+            console.log("tmp_dragged : " + $("#tmp_dragged").text());
+            console.log("btn_word : " + event.data.word);
+            console.log("whole_text : " + whole_text);
+
+            context.document.body.insertText(whole_text, 'Start');
+
+            $("#tmp_content").text(context.document.body.text);
+
             
-            context.document.getSelection().clear();
-            context.document.getSelection().insertText(event.data.word, 'Start');
             // 범위 선택 결과를 로드하는 명령을 큐에 넣습니다.
 
             return context.sync()
                 .then(function () {
+                    
 
+                    sendHistory(event.data.dragged, event.data.word);
                 });
-
         })
         .catch(errorHandler);
     }
@@ -166,28 +292,6 @@
         document.location.href = "Main.html";
     }
 
-    function tmp() {
-        Word.run(function (context) {
-
-            //var para = context.document.getSelection().paragraphs;
-            //para.load();
-
-            var documentBody = context.document.body;
-
-            context.load(documentBody, 'text');
-
-            //documentBody.clear();
-            return context.sync()
-                .then(function () {
-                    console.log("11111111111111");
-                    console.log(documentBody.text);
-                    return documentBody.text;
-
-                });
-
-        })
-        .catch(errorHandler);
-    }
 
     function vocabHistory(text) {
         $('#history_main').append("<div><ul><li>" + text + " : " + "</div></li></ul>");
@@ -195,12 +299,11 @@
 
     function showHistory() {
         Word.run(function (context) {
-
-
             return context.sync()
                 .then(function () {
-                    $('.vocab_history').css("display", "block");
 
+                    receiveHistory();
+                    $('.vocab_history').css("display", "block");
                 });
 
         })
@@ -213,7 +316,7 @@
     }
 
     function recommendSentence() {
-
+        
         Word.run(function (context) {
 
             noHistory();
@@ -221,7 +324,7 @@
             var documentBody = context.document.body;
 
             var dragged = context.document.getSelection();
-            
+
             context.load(dragged, 'text');
             
             context.document.getSelection().insertText("<31fe0826-7bb3-444d-8f2e-6ca06ae24e11>", 'Start');
@@ -235,14 +338,23 @@
 
             return context.sync()
                 .then(function () {
-                    //sendToServer(dragged.text, documentBody.text);
-                    //sendToGoogle(dragged.text);
-                    sendToNaver(dragged.text);
-                    vocabHistory(dragged.text);
+
+                    $("#tmp_content").text(documentBody.text);
+                    $("#tmp_dragged").text("<31fe0826-7bb3-444d-8f2e-6ca06ae24e11>" + dragged.text);
+                    console.log("content");
+                    console.log($("#tmp_content").text());
+                    console.log($("#tmp_dragged").text());
+
+                    getRecommendList(dragged.text);
+                    //vocabHistory(dragged.text);
                     
                     //writeFile("text.txt", dragged.text);
                     //console.log(para.items[0].clear());
                     context.document.getSelection().insertText(dragged.text, 'Start');
+
+                    sendToGoogle(dragged.text);
+
+                    sendToNaver(dragged.text);
                     
                 });
 
